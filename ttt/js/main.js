01b6772ffe2624c45ce7b9bf5903115defaff3b8
SeibelStan <seibel.stan@ya.ru>
2017-07-16 21:52:17 +0600

    Up

--- ttt/js/main.js
+++ ttt/js/main.js
@@ -13,6 +13,49 @@ function qs(selector) {
     return document.querySelectorAll(selector);
 }
 
+function showWinner() {
+    if(win) {
+        setTimeout(function() {
+            alert('Победили ' + names[+hod]);
+            newGame();
+        }, 200);
+    }
+    else {
+        win = true;
+    }
+}
+
+function getState(i, j) {
+    var cell = q('#game [data-i="' + i + '"][data-j="' + j + '"]');
+    if(cell == null) {
+        return -1;
+    }
+    if(cell.classList.length <= 1) {
+        return 0;
+    }
+    return cell.classList[1];
+}
+
+function checkWinner(i, j) {
+    var size = q('#game').rows.length;
+    var cellState = getState(i, j);
+    var currState;
+
+    for(var dir = 0; dir < 4; dir++) {
+        for(var c = Math.max(0, i - size); c < size; c++) {
+            switch(dir) {
+                case 0: { currState = getState(i, c); break; }
+                case 1: { currState = getState(c, j); break; }
+                case 2: { currState = getState(c, c); break; }
+                case 3: { currState = getState(c, size - 1 - c); break; }
+            }
+            if(currState == -1) { return false; }
+            if(currState != cellState) { win = false; break; }
+        }
+        showWinner();
+    }
+}
+
 function newGame() {
     var html = '';
     for(var i = 0; i < 3; i++) {
@@ -35,48 +78,6 @@ function newGame() {
         }
     });
 
-    function checkWinner(i, j) {
-        var size = q('#game').rows.length;
-        var cellState = getState(i, j);
-        var currState;
-
-        for(var dir = 0; dir < 4; dir++) {
-            for(var c = Math.max(0, i - size); c < size; c++) {
-                switch(dir) {
-                    case 0: { currState = getState(i, c); break; }
-                    case 1: { currState = getState(c, j); break; }
-                    case 2: { currState = getState(c, c); break; }
-                    case 3: { currState = getState(c, size - 1 - c); break; }
-                }
-                if(currState == -1) { return false; }
-                if(currState != cellState) { win = false; break; }
-            }
-            showWinner();
-        }
-    }
-}
-
-function showWinner() {
-    if(win) {
-        setTimeout(function() {
-            alert('Победили ' + names[+hod]);
-            newGame();
-        }, 200);
-    }
-    else {
-        win = true;
-    }
-}
-
-function getState(i, j) {
-    var cell = q('#game [data-i="' + i + '"][data-j="' + j + '"]');
-    if(cell == null) {
-        return -1;
-    }
-    if(cell.classList.length <= 1) {
-        return 0;
-    }
-    return cell.classList[1];
 }
 
 window.onload = function() {
